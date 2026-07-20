acee7a5e571e70d71759d288d019d9cb7474a2e3
SchoolingSocial5 <schoolingsocial5@gmail.com>
2026-07-20 10:32:07 +0100

    fix: update deploy.sh and deploy.yml to use ycapricorn-web and vps path ycapricon/web

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-echo "Deploying Capricorn Web..."
+echo "Deploying Youngcap Web..."
 npm install
 npm run build
 
@@ -11,6 +11,6 @@ cp -r public .next/standalone/
 cp -r .next/static .next/standalone/.next/
 
 # Restart/Start standalone server using PM2
-pm2 restart "capricorn-web" || pm2 start .next/standalone/server.js --name "capricorn-web"
+pm2 restart "ycapricorn-web" || pm2 start .next/standalone/server.js --name "ycapricorn-web"
 
-echo "Capricorn Web deployed successfully!"
+echo "Youngcap Web deployed successfully!"
